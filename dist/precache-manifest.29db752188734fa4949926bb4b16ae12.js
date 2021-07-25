81f55d53beb76083807ea29280ad0bda14ac8d86
Ivan Andreiev <ivan.andreiev@ffflabel.com>
2021-07-25 16:29:58 +0300

    0.0.1-beta

---
+++ dist/precache-manifest.29db752188734fa4949926bb4b16ae12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa60c0dd8f5b576dcc2",
+    "url": "/css/app.50537551.css"
+  },
+  {
+    "revision": "1a08dfe386af464cccff8199dd8d3342",
+    "url": "/img/logo.1a08dfe3.png"
+  },
+  {
+    "revision": "2c64331b59bb05c5bccac2f69f5797cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa60c0dd8f5b576dcc2",
+    "url": "/js/app.2a0d3464.js"
+  },
+  {
+    "revision": "4780a49fff9bb9e8eed9",
+    "url": "/js/chunk-560c3650.af53d25c.js"
+  },
+  {
+    "revision": "63d90ea191264c3f37c8",
+    "url": "/js/chunk-a4747e96.fc6d5b15.js"
+  },
+  {
+    "revision": "18421496fae186ee1198",
+    "url": "/js/chunk-vendors.78d5a84e.js"
+  },
+  {
+    "revision": "e248c8980bdc4986ae84f10ccc403fd9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
